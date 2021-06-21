658ccfd2d847588d0907f07fa5dc86ee6ae7e72a
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-22 00:41:30 +0530

    Add: Test Cases

--- TU_MaxFinder.java
+++ TU_MaxFinder.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 /**
  * TU_MaxFinder -- This program contains Test Cases
+ *                 Giving sample input & checking output given
+ *                 matches with expected output / not
+ *
  *
  * @author Abhishek Shigavan
  */
@@ -13,67 +16,68 @@ public class TU_MaxFinder {
  * when Max element is at 1st position
  */
     @Test
-    void whenMaxAt1Position() {
+    void whenMaxAtFirstPosition() {
 
-        Integer num1 =7, num2 =3, num3 =5;
-        Integer max_Number = MaxFinder.find_Max(num1, num2, num3);
-        Assertions.assertEquals(7,max_Number,"Validating Max Method");
+        Integer[] intArray ={9, 7, 1, 2, 5, 4};
+        Integer max_Number = MaxFinder.find_Max(intArray);
+        Assertions.assertEquals(9,max_Number,"Validating Max Method");
 
-        Float fnum1 =30f, fnum2 =3.14f, fnum3 =5.99f;
-        Float max_Float = MaxFinder.find_Max(fnum1, fnum2, fnum3);
+        Float[] floatArray ={30f, 3.14f, 5.99f, 29.99f, 27.21f};
+        Float max_Float = MaxFinder.find_Max(floatArray);
         Assertions.assertEquals(30f,max_Float,"Validating Max Method");
 
-        String str1 ="Cat", str2 ="Ball", str3 ="Apple";
-        String max_string = MaxFinder.find_Max(str1, str2, str3);
-        Assertions.assertEquals("Cat",max_string,"Validating Max Method");
+        String[] stringArray ={"Catch", "Ball", "Apple", "Cat", "Bat"};
+        String max_string = MaxFinder.find_Max(stringArray);
+        Assertions.assertEquals("Catch",max_string,"Validating Max Method");
 
-        new MaxFinder(num1, num2, num3).find_Max();
-        new MaxFinder(fnum1, fnum2, fnum3).find_Max();
-        new MaxFinder(str1, str2, str3).find_Max();
+        new MaxFinder(intArray).find_Max();
+        new MaxFinder(floatArray).find_Max();
+        new MaxFinder(stringArray).find_Max();
     }
 /**
  * Checking program gives correct max element
- * when Max element is at 2nd position
+ * when Max element is in between First & Last position
  */
     @Test
-    void whenMaxAt2Position() {
-        Integer num1 =3, num2 =7, num3 =5;
-        Integer max_Number = MaxFinder.find_Max(num1, num2, num3);
-        Assertions.assertEquals(7,max_Number,"Validating Max Method");
+    void whenMaxIsBetweenPosition() {
 
-        Float fnum1 =3.14f, fnum2 =30f, fnum3 =5.99f;
-        Float max_Float = MaxFinder.find_Max(fnum1, fnum2, fnum3);
+        Integer[] intArray ={7, 1, 2, 9, 5, 4};
+        Integer max_Number = MaxFinder.find_Max(intArray);
+        Assertions.assertEquals(9,max_Number,"Validating Max Method");
+
+        Float[] floatArray ={3.14f, 5.99f, 30F, 29.99f, 27.21f, 7.7734f};
+        Float max_Float = MaxFinder.find_Max(floatArray);
         Assertions.assertEquals(30f,max_Float,"Validating Max Method");
 
-        String str1 ="Ball", str2 ="Cat", str3 ="Apple";
-        String max_string = MaxFinder.find_Max(str1, str2, str3);
-        Assertions.assertEquals("Cat",max_string,"Validating Max Method");
+        String[] stringArray ={"Map", "Lamp", "Dog", "Watch", "Ball", "Apple", "Cat", "Bat"};
+        String max_string = MaxFinder.find_Max(stringArray);
+        Assertions.assertEquals("Watch",max_string,"Validating Max Method");
 
-        new MaxFinder(num1, num2, num3).find_Max();
-        new MaxFinder(fnum1, fnum2, fnum3).find_Max();
-        new MaxFinder(str1, str2, str3).find_Max();
+        new MaxFinder(intArray).find_Max();
+        new MaxFinder(floatArray).find_Max();
+        new MaxFinder(stringArray).find_Max();
     }
 /**
  * Checking program gives correct max element
- * when Max element is at 3rd position
+ * when Max element is at Last position
  */
     @Test
-    void whenMaxAt3Position() {
+    void whenMaxAtLastPosition() {
 
-        Integer num1 =3, num2 =5, num3 =7;
-        Integer max_Number = MaxFinder.find_Max(num1, num2, num3);
-        Assertions.assertEquals(7,max_Number,"Validating Max Method");
+        Integer[] intArray ={6, 8, 7, 1, 2, 5, 4, 9};
+        Integer max_Number = MaxFinder.find_Max(intArray);
+        Assertions.assertEquals(9,max_Number,"Validating Max Method");
 
-        Float fnum1 =5.99f, fnum2 =3.14f, fnum3 =30f;
-        Float max_Float = MaxFinder.find_Max(fnum1, fnum2, fnum3);
+        Float[] floatArray ={7.77f, 27.21f, 3.14f, 5.99f, 29.99f, 30f};
+        Float max_Float = MaxFinder.find_Max(floatArray);
         Assertions.assertEquals(30f,max_Float,"Validating Max Method");
 
-        String str1 ="Apple", str2 ="Ball", str3 ="Cat";
-        String max_string = MaxFinder.find_Max(str1, str2, str3);
-        Assertions.assertEquals("Cat",max_string,"Validating Max Method");
+        String[] stringArray ={"Catch", "Ball", "Apple", "Cat", "Bat", "Watch"};
+        String max_string = MaxFinder.find_Max(stringArray);
+        Assertions.assertEquals("Watch",max_string,"Validating Max Method");
 
-        new MaxFinder(num1, num2, num3).find_Max();
-        new MaxFinder(fnum1, fnum2, fnum3).find_Max();
-        new MaxFinder(str1, str2, str3).find_Max();
+        new MaxFinder(intArray).find_Max();
+        new MaxFinder(floatArray).find_Max();
+        new MaxFinder(stringArray).find_Max();
     }
 }
